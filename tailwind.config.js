cd59ca0282724a7db19780b93c75da0658a88ebd
Iko Raga <ikoraga@gmail.com>
2024-05-12 13:14:06 +0700

    initial commits

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#eaad1e",
+        secondary: "#004b8f",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
